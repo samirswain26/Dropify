8d17ece556a149c86a385ebc7831ae04ffe8c502
Samir Kumar Swain <samirkumarswain267@gmail.com>
2026-02-13 23:39:45 +0530

    Image kit auth initialized

---
+++ app/api/imagekit-auth/route.ts
@@ -0,0 +1,31 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import ImageKit from "imagekit";
+import { error } from "console";
+
+const imagekit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PRIVATE_KEY || "",
+  privateKey: process.env.IMAGEKIT_PUBLIC_KEY || "",
+  urlEndpoint: process.env.URL_ENDPOINT || "",
+});
+
+export async function GET() {
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      return NextResponse.json(
+        {
+          error: "Unauthorized access",
+        },
+        {
+          status: 401,
+        },
+      );
+    }
+
+    const authparams = imagekit.getAuthenticationParameters();
+    return NextResponse.json(authparams);
+  } catch (error) {
+    return NextResponse.json({error: "Failed to authenticate for imagekit"},{status: 500});
+  }
+}
